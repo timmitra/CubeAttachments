41b09306a7b96380f520749287e0b630c64d515d
Tim Mitra <tim@it-guy.com>
2024-09-24 01:50:54 -0400

    Initial Commit

---
+++ CubeAttachments/CubeAttachmentsApp.swift
@@ -0,0 +1,25 @@
+//
+// ---------------------------- //
+// Original Project: CubeAttachments
+// Created on 2024-09-24 by Tim Mitra
+// Mastodon: @timmitra@mastodon.social
+// Twitter/X: timmitra@twitter.com
+// Web site: https://www.it-guy.com
+// ---------------------------- //
+// Copyright © 2024 iT Guy Technologies. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct CubeAttachmentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
